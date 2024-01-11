8220175ebbdc918ba01432491c21f9d17961d612
Taha Hamrouni <tahah680@gmail.com>
2024-01-11 07:55:50 +0100

    array ngfor

--- src/app/Components/comp1/comp1.component.ts
+++ src/app/Components/comp1/comp1.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comp1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './comp1.component.html',
   styleUrl: './comp1.component.css'
 })
+
 export class Comp1Component {
+  table = [{name:'ali'}, {name:'ahmed'},{name:'amin'}];
+  //table = [{name:'ali'}, {name:'ahmed'}, {name:'amin'}, ];
 
 }
