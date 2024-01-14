0d43506d33c826d46c52b33407fe3bf5817fb854
Taha Hamrouni <tahah680@gmail.com>
2024-01-14 20:36:55 +0100

    more imporvements

--- src/app/Components/repbox/repbox.component.ts
+++ src/app/Components/repbox/repbox.component.ts
@@ -1,9 +1,10 @@
+// Import necessary modules and libraries
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import jsPDF from 'jspdf';
 
+// Define interfaces for Project and Worker
 interface Project {
   id: string;
   name: string;
@@ -12,13 +13,19 @@ interface Project {
   date: string;
 }
 
+interface Worker {
+  id: string;
+  name: string;
+  lastName: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-repbox',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './repbox.component.html',
   styleUrls: ['./repbox.component.css'],
-  
 })
 export class RepboxComponent implements OnInit {
   @Input() ImportedId: string = '';
@@ -27,18 +34,19 @@ export class RepboxComponent implements OnInit {
   keyObjectsArray: any[] = [];
   reportText: string = '';
   convertedReportText: string = '';
-  chosenObject: Project = {} as Project;
+  chosenProject: Project = {} as Project;
+  chosenWorker: Worker = {} as Worker;
 
   ngOnInit() {
     this.getKeywords();
     this.getObjectsNumber();
-    console.log(this.ImportedId)
+    console.log(this.ImportedId);
   }
 
   getKeywords() {
     fetch('http://127.0.0.1:8000/keywords')
-    .then(response => {return response.json()})
-    .then(data => {
+      .then(response => response.json())
+      .then(data => {
         this.keywordsArray = data;
       })
       .catch(error => this.handleError(error));
@@ -46,8 +54,8 @@ export class RepboxComponent implements OnInit {
 
   getObjectsNumber() {
     fetch('http://127.0.0.1:8000/keywordsObjects')
-    .then(response => {return response.json()})
-    .then(data => {
+      .then(response => response.json())
+      .then(data => {
         this.keyObjectsArray = data;
       })
       .catch(error => this.handleError(error));
@@ -61,42 +69,80 @@ export class RepboxComponent implements OnInit {
     pdf.save('Generate_project_report');
   }
 
-
   getProjectbyId() {
     fetch(`http://127.0.0.1:8000/projects/${this.ImportedId}`)
-    
-    .then(response => {
-      console.log('API Response:', response);
-      return response.json();
-    })
-    .then(data => {
-      this.chosenObject = data;
-      this.changeText()
-      this.DownloadPDF();
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        this.chosenProject = data;
+        this.changeText();
+        this.DownloadPDF();
+      })
+      .catch(error => this.handleError(error));
+  }
+
+  getWorkerbyId() {
+    fetch(`http://127.0.0.1:8000/workers/${this.ImportedId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.chosenWorker = data;
+        this.changeText();
+        this.DownloadPDF();
+      })
+      .catch(error => this.handleError(error));
   }
 
   changeText() {
-    this.convertedReportText = this.reportText
-
-    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_id\s*}}/g, this.chosenObject.id);
-    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_name\s*}}/g, this.chosenObject.name);
-    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_budget\s*}}/g, this.chosenObject.budget);
-    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_category\s*}}/g, this.chosenObject.category);
-    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_date\s*}}/g, this.chosenObject.date.substring(0,10));
+    this.convertedReportText = this.reportText;
+    if (parseInt(this.ImportedId) / 10000 >= 1) {
+      this.deleteProjectKeywords();
+      this.replaceWorkerKeyWords();
+    } else {
+      this.deleteWorkerKeywords();
+      this.replaceProjectKeyWords();
+    }
   }
-
-
 
   private handleError(error: any) {
     console.error('Error fetching data:', error);
   }
 
-  savePdf()
-  {
-    this.getProjectbyId()
+  savePdf() {
+    if (parseInt(this.ImportedId) / 10000 >= 1) {
+      this.getWorkerbyId();
+      console.log('case1');
+    } else {
+      console.log('case2');
+      this.getProjectbyId();
+    }
+  }
+
+  replaceWorkerKeyWords() {
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_id\s*}}/g, this.chosenWorker.id);
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_name\s*}}/g, this.chosenWorker.name);
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_last_name\s*}}/g, this.chosenWorker.lastName);
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_title\s*}}/g, this.chosenWorker.title);
+  }
+
+  replaceProjectKeyWords() {
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_id\s*}}/g, this.chosenProject.id);
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_name\s*}}/g, this.chosenProject.name);
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_budget\s*}}/g, this.chosenProject.budget);
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_category\s*}}/g, this.chosenProject.category);
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_date\s*}}/g, this.chosenProject.date);
+  }
+
+  deleteWorkerKeywords() {
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_id\s*}}/g, '');
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_name\s*}}/g, '');
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_last_name\s*}}/g, '');
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*worker_title\s*}}/g, '');
+  }
+
+  deleteProjectKeywords() {
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_id\s*}}/g, '');
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_name\s*}}/g, '');
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_budget\s*}}/g, '');
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_category\s*}}/g, '');
+    this.convertedReportText = this.convertedReportText.replace(/{{\s*project_date\s*}}/g, '');
   }
 }
